b4c0a03d1364eb492a9fba7c8d851b7c5f31bda3
dev <dev@brigid.jp>
2021-09-23 15:56:30 +0900

    HTMLとJSを修正。

--- pub/recorder.js
+++ pub/recorder.js
@@ -1,22 +1,93 @@
 onload = () => {
+  let stream
+  let video_track
+  let audio_track
+  let devices
 
-  let log = (v) => {
-    let e = document.getElementById("log")
-    e.textContent += v + "\n"
-    console.log(v)
+  let log = (...args) => {
+    console.log(args)
+    document.getElementById("log").textContent += args.join(" ") + "\n"
   };
 
-  (async () => {
-    let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-    // console.log(stream)
-    log(stream)
-    let devices = await navigator.mediaDevices.enumerateDevices()
-    for (let i = 0; i < devices.length; ++i) {
-      let device = devices[i]
-      // console.log(device)
-      log(device)
+  let create_element = (name, attributes, values) => {
+    let element = document.createElement(name)
+    Object.entries(attributes).forEach(([key, value]) => {
+      if (value) {
+        element.setAttribute(key, value)
+      }
+    })
+    if (values) {
+      values.forEach((value) => {
+        if (value.nodeType) {
+          element.appendChild(value)
+        } else {
+          element.appendChild(document.createTextNode(value))
+        }
+      })
     }
+    return element
+  }
+
+  (async () => {
+    stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    devices = await navigator.mediaDevices.enumerateDevices()
+
+    stream.getVideoTracks().forEach((track, i) => {
+      if (i == 0) {
+        video_track = track
+      }
+      log("video track", track.kind, track.id, track.label, track.getSettings().deviceId)
+    })
+    stream.getAudioTracks().forEach((track, i) => {
+      if (i == 0) {
+        audio_track = track
+      }
+      log("audio track", track.kind, track.id, track.label, track.getSettings().deviceId)
+    })
+
+    devices.forEach((device) => {
+      log("device", device.kind, device.deviceId, device.label)
+
+      let selector_name
+      let selector
+      let checked
+      if (device.kind === "videoinput") {
+        selector_name = "video-selector"
+        selector = document.getElementById("video-selector")
+        checked = device.deviceId === video_track.getSettings().deviceId
+      } else if (device.kind == "audioinput") {
+        selector_name = "audio-selector"
+        selector = document.getElementById("audio-selector")
+        checked = device.deviceId === audio_track.getSettings().deviceId
+      } else {
+        return
+      }
+
+      let id = device.kind + "-" + device.deviceId
+
+      selector.appendChild(create_element("div", {}, [
+        create_element("input", {
+          type: "radio",
+          id: id,
+          name: selector_name,
+          value: device.deviceId,
+          checked: checked,
+        }),
+        " ",
+        create_element("label", { "for": id, }, [ device.label ]),
+      ]))
+    })
+
+    // document.getElementById("audio-selector").appendChild(
+    //   create_element("div", {}, [
+    //     "test1",
+    //     create_element("input", { type: "text", value: "あああ" }),
+    //     "test2",
+    //   ]))
+
+    // create_element("input", { type: "text", "data-test": 42 }, [])
+
   })().catch((e) => {
-    console.log(e)
+    log(e)
   })
 }
