5d3b332db53b4cfb5cfc7234d8fbdb9f7d3815fd
dev <dev@brigid.jp>
2021-09-23 21:18:37 +0900

    HTMLとJSを修正。

--- pub/recorder.js
+++ pub/recorder.js
@@ -1,11 +1,34 @@
 addEventListener("DOMContentLoaded", () => {
   let stream
   let recorder
+  let session
+  let session_counter
+  let authorization
 
   let log = (...args) => {
     console.log(args)
     document.getElementById("log").textContent += args.join(" ") + "\n"
-  };
+  }
+
+  let format = (width, value) => {
+    let s = value.toString()
+    let w = width - s.length
+    if (w > 0) {
+      return "0".repeat(w) + s
+    } else {
+      return s
+    }
+  }
+
+  let format_date = date => {
+    return date.getFullYear() +
+      format(2, date.getMonth()) +
+      format(2, date.getDay()) + "_" +
+      format(2, date.getHours()) +
+      format(2, date.getMinutes()) +
+      format(2, date.getSeconds()) + "_" +
+      format(3, date.getMilliseconds())
+  }
 
   let create_element = (name, attributes, values) => {
     let element = document.createElement(name)
@@ -57,12 +80,48 @@ addEventListener("DOMContentLoaded", () => {
     update_video()
   }
 
+  let upload = async data => {
+    let suffix
+    if (data.type.search(/video\/mp4/) !== -1) {
+      suffix = ".mp4"
+    } else if (data.type.search(/video\/webm/) !== -1) {
+      suffix = ".webm"
+    } else if (data.type.search(/video\/x-matroska/) !== -1) {
+      suffix = ".mkv"
+    } else {
+      suffix = ".dat"
+    }
+
+    let path = "/dav/" + session + "-" + format(8, ++session_counter) + suffix
+
+    let started = new Date()
+    let response = await fetch(path, {
+      method: "PUT",
+      headers: {
+        "Content-Type": data.type,
+        Authorization: authorization,
+      },
+      body: data,
+    })
+    let finished = new Date()
+
+    log(path, response.status, response.statusText, finished.getTime() - started.getTime())
+  }
+
   let start = async () => {
+    session = format_date(new Date()) + "-" + format(6, Math.floor(Math.random() * 999999))
+    session_counter = 0
+
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    authorization = "Basic " + btoa(username + ":" + password)
+
     log("start")
     recorder = new MediaRecorder(stream)
     recorder.ondataavailable = ev => {
       let data = ev.data
       log("data", data.type, data.size)
+      upload(data).catch(e => log(e))
     }
     recorder.start(1000)
   }
