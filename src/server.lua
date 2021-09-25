05bd97c9272f57cc1e259e458398ee42205f6d67
dev <dev@brigid.jp>
2021-09-25 11:53:07 +0900

    WebSocketサーバの実装を修正。

--- src/server.lua
+++ src/server.lua
@@ -28,6 +28,19 @@ while true do
   local selected_sockets = assert(socket.select(recvt, sendt, timeout))
 
   seq_no = seq_no + 1
+
+  if seq_no % 10 == 0 then
+    print "ping"
+    for s in pairs(accepted_sockets) do
+      s:send "\x89\x04ping"
+    end
+  elseif seq_no % 10 == 2 then
+    print "text"
+    for s in pairs(accepted_sockets) do
+      s:send "\x81\x04text"
+    end
+  end
+
   print(seq_no, #selected_sockets, #recvt)
   for i = 1, #selected_sockets do
     local s = selected_sockets[i]
@@ -51,6 +64,7 @@ while true do
               io.write(line)
               state.line = nil
               state.method, state.uri, state.version = line:match "(%S+)%s+(%S+)%s+(%S+)"
+              state.header_fields = ""
               state.mode = 2
             else
               state.line = line
@@ -73,6 +87,7 @@ while true do
                 end
               end
               state.headers = headers
+              state.frame = ""
               state.mode = 3
 
               print(state.method)
@@ -105,9 +120,80 @@ Sec-WebSocket-Accept: %s
             else
               state.header_fields = header_fields
             end
-
           elseif state.mode == 3 then
-            -- ????
+            local frame = state.frame .. result
+            if #frame == 2 then
+              local a, b = frame:byte(1, 2)
+              print(("0x%0x 0x%0x"):format(a, b))
+              state.frame = nil
+              state.f = {}
+              state.f.fin = a & 0x80 ~= 0
+              state.f.rsv1 = a & 0x40 ~= 0
+              state.f.rsv2 = a & 0x20 ~= 0
+              state.f.rsv3 = a & 0x10 ~= 0
+              state.f.opcode = a & 0x0F
+              state.f.mask = b & 0x80 ~= 0
+              state.f.length = b & 0x7F
+              state.f.mask_data = {}
+              state.f.mask_n = 4
+              state.payload = ""
+              print(state.f.fin, state.f.opcode, state.f.mask, state.f.length)
+              if state.f.length == 126 then
+                state.f.length = 0
+                state.f.len_n = 2
+                state.mode = 4
+              elseif state.f.length == 127 then
+                state.f.length = 0
+                state.f.len_n = 8
+                state.mode = 4
+              else
+                state.mode = 5
+              end
+            else
+              state.frame = frame
+            end
+          elseif state.mode == 4 then
+            state.f.length = state.f.length * 256 + result:byte()
+            state.f.len_n = state.f.len_n - 1
+            if state.f.len_n == 0 then
+              state.mode = 5
+            end
+          elseif state.mode == 5 then
+            local mask_data = state.f.mask_data
+            mask_data[#mask_data + 1] = result:byte()
+            state.f.mask_n = state.f.mask_n - 1
+            if state.f.mask_n == 0 then
+              print(("0x%02x 0x%02x 0x%02x 0x%02x"):format(mask_data[1], mask_data[2], mask_data[3], mask_data[4]))
+              state.f.n = state.f.length
+              state.f.m = 0
+              state.payload = ""
+              if state.f.length == 0 then
+                state.mode = 7
+              else
+                state.mode = 6
+              end
+            end
+          elseif state.mode == 6 then
+            state.payload = state.payload .. string.char(state.f.mask_data[state.f.m + 1] ~ result:byte())
+            state.f.m = (state.f.m + 1) % 4
+            state.f.n = state.f.n - 1
+            if state.f.n == 0 then
+              state.mode = 7
+            end
+          end
+
+          if state.mode == 7 then
+            print("<" .. state.payload .. ">")
+
+            if state.f.opcode == 0x8 then
+              print "<CLOSE>"
+              s:send "\x88\x00"
+              s:close()
+              accepted_sockets[s] = nil
+            end
+
+            state.frame = ""
+            state.mode = 3
           end
         else
           print("[" .. message .. "]")
