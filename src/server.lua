aebcfd2f216fd9a3db0d8ddbc68263d964275e4c
dev <dev@brigid.jp>
2021-09-27 13:11:32 +0900

    WebSocketサーバの実装を修正。

--- src/server.lua
+++ src/server.lua
@@ -8,11 +8,19 @@ local socket = require "socket"
 local service = require "service"
 local websocket = require "websocket"
 
+local select_timer = 1
+local ping_timer = 10
+
 local host, serv = ...
 
 local service = service()
 local server = assert(socket.bind(host, serv))
 assert(server:settimeout(0))
+
+local function ping(ws)
+  ws:send_ping()
+  ws.ping_timer = service:add_timer(function () ping(ws) end, ping_timer)
+end
 
 while true do
   local recv = { server }
@@ -22,8 +30,8 @@ while true do
     recv[#recv + 1] = s
   end
 
-  local recv, send, message = assert(socket.select(recv, send, 1))
-  local now = os.time()
+  local recv, send, message = assert(socket.select(recv, send, select_timer))
+  service:update_timer()
 
   for i = 1, #recv do
     local s = recv[i]
@@ -37,6 +45,9 @@ while true do
       end
       function ws:on_close(host, serv, family)
         io.write(("on_close[%s,%s,%s]\n"):format(host, serv, family))
+        if self.ping_timer then
+          service:remove_timer(self.ping_timer)
+        end
       end
       function ws:on_text()
         io.write(("on_text[opcode=0x%X][%s]\n"):format(self.opcode, self.payload))
@@ -47,6 +58,8 @@ while true do
       function ws:on_pong()
         io.write(("on_pong[opcode=0x%X][%s]\n"):format(self.opcode, self.payload))
       end
+
+      ws.ping_timer = service:add_timer(function () ping(ws) end, ping_timer)
     else
       local ws = service:get_socket(s)
       local result, message, data = s:receive(4096)
--- src/service.lua
+++ src/service.lua
@@ -2,6 +2,8 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/mit-license.php
 
+local socket = require "socket"
+
 local class = {}
 local metatable = { __index = class }
 
@@ -9,6 +11,8 @@ local function new()
   return {
     sockets = {};
     timers = {};
+    timer_handle = 0;
+    current_time = socket.gettime();
   }
 end
 
@@ -26,6 +30,30 @@ end
 
 function class:each_socket()
   return next, self.sockets
+end
+
+function class:update_timer()
+  self.current_time = socket.gettime()
+
+  for handle, timer in pairs(self.timers) do
+    if timer.t <= self.current_time then
+      timer.f()
+      self.timers[handle] = nil
+    end
+  end
+end
+
+function class:add_timer(f, timeout)
+  self.timer_handle = self.timer_handle + 1
+  self.timers[self.timer_handle] = {
+    f = f;
+    t = self.current_time + timeout;
+  }
+  return self.timer_handle
+end
+
+function class:remove_timer(handle)
+  self.timers[handle] = nil
 end
 
 return setmetatable(class, {
