11df4ebf091fba647cf2d96b6c3f65fdfa716c92
dev <dev@brigid.jp>
2021-09-27 14:17:06 +0900

    HTMLとJSを修正。

---
+++ pub/control.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 <dev@brigid.jp>
+// This software is released under the MIT License.
+// https://opensource.org/licenses/mit-license.php
+
+addEventListener("DOMContentLoaded", () => {
+})
